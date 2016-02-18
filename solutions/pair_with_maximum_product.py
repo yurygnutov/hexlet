a0c0c26064401dc6f1fde5dd5461b7e9f7c833e7
Yury Gnutov <yury.gnutov@booming-games.com>
2016-02-18 15:03:45 +0100

    recommit

--- solutions/pair_with_maximum_product.py
+++ solutions/pair_with_maximum_product.py
@@ -1,1 +1,17 @@
 __author__ = 'yury'
+
+
+# Given an array with both positive and negative integers,
+# return a pair of integers that, when multiplied,
+# would produce the largest possible number.
+# An element can only be used once, in other words, you cannot multiply a number by itself.
+# Elements of the pair should be arranged in ascending order.
+
+# Example:
+# [-4,-3] == solution([-1,-2,-4,-3,0,3,2,1])
+
+from itertools import permutations
+
+def solution(seq):
+    pairs = list(permutations(seq, 2))
+    return sorted(list(sorted(pairs, key=lambda x: x[0] * x[1])[-1]))
--- solutions/ruth_aaron_pairs.py
+++ solutions/ruth_aaron_pairs.py
@@ -1,1 +1,25 @@
 __author__ = 'yury'
+
+# Check if a given pair of numbers is a Ruth–Aaron pair.
+# A Ruth–Aaron pair consists of two consecutive integers (e.g. 714 and 715)
+# for which the sums of the prime factors of each integer are equal.
+# When calculating the sum, take into account the repeating factors.
+
+# Example:
+# true == solution([8,9])
+
+
+def pf(n):
+    pf = []
+    d = 2
+    while d ** 2 <= n:
+        while (n % d) == 0:
+            pf.append(d)
+            n //= d
+        d += 1
+    if n > 1:
+       pf.append(n)
+    return pf
+
+def solution(pair):
+    return sum(pf(pair[0])) == sum(pf(pair[1]))
