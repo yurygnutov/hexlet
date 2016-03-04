1f2aaee164612c5a9a6cd167e410cf63b1e1cf9a
Yury Gnutov <yury.gnutov@booming-games.com>
2016-03-04 21:32:05 +0100

    simplified

--- solutions/find_palindrome.py
+++ solutions/find_palindrome.py
@@ -11,19 +11,12 @@ def is_palindrome(x):
     return x == x[::-1]
 
 
-def split_subsequences(iterable, length=2, overlap=0):
-    it = iter(iterable)
-    results = list(itertools.islice(it, length))
-    while len(results) == length:
-        yield results
-        results = results[length - overlap:]
-        results.extend(itertools.islice(it, length - overlap))
-    if results:
-        yield results
+def each_cons(x, size):
+    return [x[i:i+size] for i in range(len(x)-size+1)]
 
 
 def solution(t):
     for k in range(len(t), 0, -1):
-        for word in split_subsequences(t, k, k - 1):
+        for word in each_cons(t, k):
             if is_palindrome("".join(word)):
                 return "".join(word)
