a9c6f41b8b677a961c838b34b9142dc519d00477
Yury Gnutov <yury.gnutov@booming-games.com>
2016-03-30 20:07:32 +0200

    new solution for pair_with_maximum_product.py

--- solutions/pair_with_maximum_product.py
+++ solutions/pair_with_maximum_product.py
@@ -11,7 +11,13 @@ __author__ = 'yury'
 # [-4,-3] == solution([-1,-2,-4,-3,0,3,2,1])
 
 from itertools import permutations
+from functools import reduce
+import operator
 
-def solution(seq):
+def solution_old(seq):
     pairs = list(permutations(seq, 2))
     return sorted(list(sorted(pairs, key=lambda x: x[0] * x[1])[-1]))
+
+
+def solution(seq):
+    return max(permutations(seq, 2), key=lambda x: reduce(operator.mul, x))
